ddebe9c6b721a51fc5b65f2b7eeaa874bef51fcc
MOREL JAMES p2104270 <james.morel@etu.univ-lyon1.fr>
2022-11-16 14:39:28 +0100

    revue du code + prototipe de carte avec l'image qui s'affiche

--- public/index.js
+++ public/index.js
@@ -1,7 +1,9 @@
 let canvas;
 let context;
 let palteau;
-let elements =[];
+let elements =[]; //tableau contenant les elements qui peuvent être cliqué à la souris
+
+let drawElement =[];
 
 window.onload = function () {
     canvas = document.querySelector('canvas');
@@ -9,9 +11,29 @@ window.onload = function () {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
 
-    palteau = new Plateau();
-    palteau.animate();
+    plateau = new Plateau();
+    drawElement.push(plateau);
+    //palteau.animate();
+
+    carte = new Carte('./images/boo.jpg',50,50,100,200);
+    drawElement.push(carte);
+
+    drawAll();
+    //carte.animate(50,50,100,200);
+
+    /*var img = new Image();
+    img.onload = ()=>{
+        context.drawImage(img,0,0);
+    };
+    img.src = './boo.jpg';*/
     
+    
+}
+
+function drawAll(){
+    context.clearRect(0,0,canvas.width,canvas.height);
+    drawElement.forEach(elem => elem.draw())
+    requestAnimationFrame(drawAll);
 }
 
 window.addEventListener('resize',function(){
@@ -49,12 +71,30 @@ class Plateau {
         }
     }
 
-    #draw(){
+    draw(){
         context.fillStyle = 'brown';
+
+        this.width = canvas.width/2;
+        this.height = canvas.height/2;
+        this.x = canvas.width/6;
+        this.y = canvas.height/5;
+
         console.log(this.width);
         context.fillRect(this.x, this.y, this.width, this.height);
+
+
+        let xtmp = this.x;
+        for (let i = 0; i < 8; i++) {
+            
+            this.listeEmplacements[i].draw(xtmp);
+            xtmp+=this.width/4;
+            if (i===3) {
+                xtmp = this.x;
+            }
+            
+        }
     }
-    animate(){
+    /*animate(){
         this.width = canvas.width/2;
         this.height = canvas.height/2;
         this.x = canvas.width/6;
@@ -64,7 +104,7 @@ class Plateau {
         let xtmp = this.x;
         for (let i = 0; i < 8; i++) {
             
-            this.listeEmplacements[i].animate(xtmp);
+            this.listeEmplacements[i].draw(xtmp);
             xtmp+=this.width/4;
             if (i===3) {
                 xtmp = this.x;
@@ -72,7 +112,7 @@ class Plateau {
             
         }
         requestAnimationFrame(this.animate.bind(this));
-    }
+    }*/
 }
 
 class Emplacement{
@@ -86,25 +126,70 @@ class Emplacement{
         this.#y = y;
         this.#width = canvas.width/8;
         this.#height = canvas.height /6;
-        this.#draw();
+        //this.#draw();
     }
 
-    #draw(){
+    draw(x){
+        this.#x=x;
+        this.#width = canvas.width/8;
+        this.#height = canvas.height /6;
         context.fillStyle = 'white';
         context.strokeRect(this.#x, this.#y, this.#width, this.#height);
     }
-    animate(x){
+    /*animate(x){
         this.#x = x;
         //this.#y = y;
         this.#width = canvas.width/8;
         this.#height = canvas.height /6;
         this.#draw();
         //requestAnimationFrame(this.animate.bind(this));
-    }
+    }*/
     setX(x){
         this.#x = x;
     }
     setY(y){
         this.#y = y;
     }
+}
+
+class Carte{
+    #img;
+    #x;
+    #y;
+    #width;
+    #height;
+
+    constructor(imgSrc,x,y,width,height) {
+        this.#img = new Image();
+        this.#img.src = imgSrc;
+        console.log(this.#img.src);
+        /*this.#img.onload = ()=>{
+            context.drawImage(this.#img,0,0);
+        };*/
+        this.#x = x;
+        this.#y = y;
+        this.#width = width;
+        this.#height = height;
+        
+    }
+
+    draw(){
+        
+        console.log(this.#x);
+        console.log(this.#y);
+
+        context.fillStyle = 'green';
+        context.fillRect(this.#x,this.#y,this.#width,this.#height);
+        context.drawImage(this.#img,this.#x,this.#y,this.#width,this.#height/3);
+        /*this.#img.onload = ()=>{
+            console.log(this.#x);
+            console.log(this.#y);
+            
+        };*/
+    }
+
+    /*animate(x,y,width,height){
+        this.#draw(x,y,width,height);
+        requestAnimationFrame(this.animate.bind(this))
+    }*/
 }
